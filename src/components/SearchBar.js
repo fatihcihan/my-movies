7a80f39f543bafd15f3cb69d766aa62c7634d7d3
Fatih <fatihcihan666@gmail.com>
2023-04-13 02:58:56 +0300

    Controlled component overview. Thanks to preventDefault, the page is prevented from being refreshed

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,28 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+
+    state = {
+        searchQuery: ""
+    }
+
+    handleFormSubmit = (event) => {
+        event.preventDefault();
+    }
+
     render() {
         return (
-            <h1>
-                Search Bar
-            </h1>
+            <form onSubmit={this.handleFormSubmit}>
+                <div className="form-row mb-3 mt-2">
+                    <div className="col-12">
+                        <input
+                            onChange={(event) => this.setState({ searchQuery: event.target.value })}
+                            type="text" className="form-control" placeholder="Search a movie"
+                            value={this.state.searchQuery} />
+                    </div>
+                </div>
+            </form>
+
         );
     }
 }
