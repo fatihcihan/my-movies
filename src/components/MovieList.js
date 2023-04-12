b6c2e50802c2df310aeb42b3f5653553df6bcc52
Fatih <fatihcihan666@gmail.com>
2023-04-13 01:43:04 +0300

    Class component changed to functional component

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,31 +1,33 @@
 import React from 'react';
 
-class MovieList extends React.Component {
-    render() {
-        return (
-            <div className="row">
+const MovieList = (props) => {
 
-                {this.props.movies.map((movie) => (
-                    <div className="col-lg-4" key={movie.id}>
-                        <div className="card mb-4 shadow-sm">
-                            <img className="card-img-top" alt='Sample Movie' src={movie.imageUrl} />
-                            <div className="card-body">
-                                <h5 className='card-title'>{movie.name}</h5>
-                                <p className='card-text'>{movie.overview}</p>
-                                <div className="d-flex justify-content-between align-items-center">
-                                    <button className='btn btn-outline-danger' type="button">Delete</button>
-                                    <h2><span className='border-right'>{movie.rating}</span></h2>
-                                </div>
+    /* function handleClick(e) {
+        console.log(e);
+    } */
+
+    return (
+        <div className="row">
+
+            {props.movies.map((movie) => (
+                <div className="col-lg-4" key={movie.id}>
+                    <div className="card mb-4 shadow-sm">
+                        <img className="card-img-top" alt='Sample Movie' src={movie.imageUrl} />
+                        <div className="card-body">
+                            <h5 className='card-title'>{movie.name}</h5>
+                            <p className='card-text'>{movie.overview}</p>
+                            <div className="d-flex justify-content-between align-items-center">
+                                <button onClick={(event) => {props.deleteMovieProp(movie) }} className='btn btn-outline-danger' type="button">Delete</button>
+                                <h2><span className='border-right'>{movie.rating}</span></h2>
                             </div>
                         </div>
                     </div>
+                </div>
 
-                ))}
+            ))}
 
-
-            </div>
-        );
-    }
+        </div>
+    );
 }
 
 
