a6ab7960eb172a23d3bb91be2e774490365f824e
Fatih <fatihcihan666@gmail.com>
2023-04-14 20:49:51 +0300

    Add movie button added

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,12 +9,19 @@ class SearchBar extends React.Component {
     render() {
         return (
             <form onSubmit={this.handleFormSubmit}>
-                <div className="form-row mb-3 mt-2">
-                    <div className="col-12">
+                <div className="row mb-3 mt-2">
+                    <div className="col-10">
                         <input
                             onChange={this.props.searchMovieProp}
                             type="text" className="form-control" placeholder="Search a movie"
                         />
+                    </div>
+                    <div className="col-2">
+                        <button type="button"
+                            className="btn btn-primary"
+                            style={{ float: "right" }}
+                        >Add Movie
+                        </button>
                     </div>
                 </div>
             </form>
