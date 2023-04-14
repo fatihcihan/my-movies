dbe1246745e5549fc6dc01e9dbf23cc8c0ef218a
Fatih <fatihcihan666@gmail.com>
2023-04-14 20:40:24 +0300

    Add movie class component added

---
+++ src/components/AddMovie.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AddMovie extends React.Component {
+
+    render() {
+        return (
+            <div className="container">
+                <div>Add Movie Component</div>
+            </div>
+        );
+    }
+}
+
+export default AddMovie;
