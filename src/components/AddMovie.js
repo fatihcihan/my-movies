cefefd5f29be9ba27df482c8318f27bedbc90c0c
Fatih <fatihcihan666@gmail.com>
2023-04-15 19:07:17 +0300

    Form added

--- src/components/AddMovie.js
+++ src/components/AddMovie.js
@@ -5,7 +5,35 @@ class AddMovie extends React.Component {
     render() {
         return (
             <div className="container">
-                <div>Add Movie Component</div>
+                <form className="mt-5">
+                    <div className="row mb-3 mt-2">
+                        <div className="col-10">
+                            <div className="mb-3">
+                                <label className="form-label">Name</label>
+                                <input type="text" className="form-control" placeholder="Enter name" />
+                            </div>
+                        </div>
+                        <div className="col-2">
+                            <div className="mb-3">
+                                <label className="form-label">Rating</label>
+                                <input type="number" className="form-control" placeholder="Enter rating" />
+                            </div>
+                        </div>
+                        <div className="col-12">
+                            <div className="mb-3">
+                                <label className="form-label">Image URL</label>
+                                <input type="text" className="form-control" placeholder="Enter image url" />
+                            </div>
+                        </div>
+                        <div className="col-12">
+                            <div className="mb-3">
+                                <label className="form-label">Overview</label>
+                                <textarea className="form-control" rows="5" placeholder="Enter overview"></textarea>
+                            </div>
+                            <input type="submit" className="btn btn-success btn-block" value="Add Movie" />
+                        </div>
+                    </div>
+                </form>
             </div>
         );
     }
