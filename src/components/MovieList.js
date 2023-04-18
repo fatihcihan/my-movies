2ad17e16a1a64b5ad107176abf0fe0d812a2f90a
Fatih <fatihcihan666@gmail.com>
2023-04-18 14:15:14 +0300

    i parameter is sent to map method to fix unique key error

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -9,15 +9,15 @@ const MovieList = (props) => {
     return (
         <div className="row">
 
-            {props.movies.map((movie) => (
-                <div className="col-lg-4" key={movie.id}>
+            {props.movies.map((movie, i) => (
+                <div className="col-lg-4" key={i}>
                     <div className="card mb-4 shadow-sm">
                         <img className="card-img-top" alt='Sample Movie' src={movie.imageUrl} />
                         <div className="card-body">
                             <h5 className='card-title'>{movie.name}</h5>
                             <p className='card-text'>{movie.overview}</p>
                             <div className="d-flex justify-content-between align-items-center">
-                                <button onClick={(event) => {props.deleteMovieProp(movie) }} className='btn btn-outline-danger' type="button">Delete</button>
+                                <button onClick={(event) => { props.deleteMovieProp(movie) }} className='btn btn-outline-danger' type="button">Delete</button>
                                 <h2><span className='border-right'>{movie.rating}</span></h2>
                             </div>
                         </div>
