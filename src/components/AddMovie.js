04a396f09566a4343faa4bee5bc7ae91264f6669
Fatih <fatihcihan666@gmail.com>
2023-04-18 14:17:04 +0300

    Serialize imported, new movie object sent to parent component

--- src/components/AddMovie.js
+++ src/components/AddMovie.js
@@ -1,34 +1,42 @@
 import React from 'react';
+import serialize from 'form-serialize'
 
 class AddMovie extends React.Component {
+
+    handleFormSubmit = (e) => {
+        e.preventDefault();
+        const newMovie = serialize(e.target, { hash: true });
+        console.log(newMovie);
+        this.props.onAddMovie(newMovie);
+    }
 
     render() {
         return (
             <div className="container">
-                <form className="mt-5">
+                <form className="mt-5" onSubmit={this.handleFormSubmit}>
                     <div className="row mb-3 mt-2">
                         <div className="col-10">
                             <div className="mb-3">
                                 <label className="form-label">Name</label>
-                                <input type="text" className="form-control" placeholder="Enter name" />
+                                <input name="name" type="text" className="form-control" placeholder="Enter name" />
                             </div>
                         </div>
                         <div className="col-2">
                             <div className="mb-3">
                                 <label className="form-label">Rating</label>
-                                <input type="number" className="form-control" placeholder="Enter rating" />
+                                <input name="rating" type="number" className="form-control" placeholder="Enter rating" />
                             </div>
                         </div>
                         <div className="col-12">
                             <div className="mb-3">
                                 <label className="form-label">Image URL</label>
-                                <input type="text" className="form-control" placeholder="Enter image url" />
+                                <input name="imageUrl" type="text" className="form-control" placeholder="Enter image url" />
                             </div>
                         </div>
                         <div className="col-12">
                             <div className="mb-3">
                                 <label className="form-label">Overview</label>
-                                <textarea className="form-control" rows="5" placeholder="Enter overview"></textarea>
+                                <textarea name="overview" className="form-control" rows="5" placeholder="Enter overview"></textarea>
                             </div>
                             <input type="submit" className="btn btn-success btn-block" value="Add Movie" />
                         </div>
