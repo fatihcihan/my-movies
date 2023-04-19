41a773530c0f183f44db2faa51dddc7aa8cbf7d3
Fatih <fatihcihan666@gmail.com>
2023-04-19 18:11:23 +0300

    Truncate operation done for movie overview

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,9 +2,18 @@ import React from 'react';
 
 const MovieList = (props) => {
 
-    /* function handleClick(e) {
-        console.log(e);
-    } */
+
+    const truncateOverview = (string, maxLength) => {
+        if (!string) {
+            return null;
+        }
+        if (string.length <= maxLength) {
+            return string;
+        }
+        else {
+            return `${string.substring(0, maxLength)} ...`;
+        }
+    }
 
     return (
         <div className="row">
@@ -15,7 +24,7 @@ const MovieList = (props) => {
                         <img className="card-img-top" alt='Sample Movie' src={movie.imageUrl} />
                         <div className="card-body">
                             <h5 className='card-title'>{movie.name}</h5>
-                            <p className='card-text'>{movie.overview}</p>
+                            <p className='card-text'>{truncateOverview(movie.overview, 50)}</p>
                             <div className="d-flex justify-content-between align-items-center">
                                 <button onClick={(event) => { props.deleteMovieProp(movie) }} className='btn btn-outline-danger' type="button">Delete</button>
                                 <h2><span className='border-right'>{movie.rating}</span></h2>
